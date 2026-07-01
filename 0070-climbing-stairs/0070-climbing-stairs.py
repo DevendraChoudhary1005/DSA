dde7b487ae9749f40b4eede0cb7ae8b087f1c71e
Devendra Choudhary <145139188+DevendraChoudhary1005@users.noreply.github.com>
2026-07-01 09:50:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def helper(steps):
+            if steps<=2:
+                return steps
+            if steps in memo:
+                return memo[steps]
+
+            memo[steps] = helper(steps-1) + helper(steps-2)
+            return memo[steps]
+
+        return helper(n)
