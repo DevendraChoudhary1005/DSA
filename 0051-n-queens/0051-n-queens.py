c0e186f26ec76cb2ac6f7979a942eb5dd6051e1f
Devendra Choudhary <145139188+DevendraChoudhary1005@users.noreply.github.com>
2026-07-05 15:29:16 +0530

    Time: 7 ms (96.44%), Space: 19.5 MB (98.41%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,5 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        # Now using Hash Map
+        def solve(col, board, ans, leftrow, upperDiagonal, lowerDiagonal, n):
+            if col == n:
+                ans.append(board[:])
+                return
+
+            for row in range(n):
+                if (
+                    leftrow[row] == 0
+                    and lowerDiagonal[row+col] == 0
+                    and upperDiagonal[n-1 + col - row] == 0):
+                    board[row] = board[row][:col] + "Q" + board[row][col+1:]
+                    leftrow[row] = 1
+                    lowerDiagonal[row+col] = 1
+                    upperDiagonal[n-1+col-row] = 1
+                    solve(col+1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+                    board[row] = board[row][:col] + "." + board[row][col+1:]
+                    leftrow[row] = 0
+                    lowerDiagonal[row+col] = 0
+                    upperDiagonal[n-1 + col - row] = 0
+
+        ans = []
+        board = ["."*n for _ in range(n)]
+        leftrow = [0]*n
+        upperDiagonal = [0]*(2*n-1)
+        lowerDiagonal = [0]*(2*n-1)
+        solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n)
+        return ans
+        
+        """Solved USing Recursion
         board = ["."*n for _ in range(n)]
         ans = []
 
@@ -43,4 +73,4 @@ class Solution:
                     solve(col+1, board, n)
                     board[row] = board[row][:col] + "." + board[row][col+1:]
         solve(0, board, n)
-        return ans
+        return ans"""
