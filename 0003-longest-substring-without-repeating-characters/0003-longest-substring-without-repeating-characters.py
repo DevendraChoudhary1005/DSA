0f6cc779bd487e6f0dc0679e279eac614947f35c
Devendra Choudhary <145139188+DevendraChoudhary1005@users.noreply.github.com>
2026-08-16 15:52:42 +0530

    Time: 211 ms (22.6%), Space: 19.9 MB (24.93%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,6 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
+        """max_len = 0
         n = len(s)
 
         for i in range(0, n):
@@ -10,6 +10,22 @@ class Solution:
                     break
                 max_len = max(max_len, j-i+1)
                 charset.add(s[j])
+
+        return max_len"""
+
+        my_dict = {}
+        n = len(s)
+        l = 0
+        r = 0
+        max_len = 0
+
+        while r<n:
+            if s[r] in my_dict:
+                l = max(l, my_dict[s[r]]+1)
+
+            max_len = max(max_len, r-l+1)
+            my_dict[s[r]] = r
+            r += 1
 
         return max_len
         
