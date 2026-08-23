a4c5e78d2754a8ad87680f3cff03bb92bf618ad2
Devendra Choudhary <145139188+DevendraChoudhary1005@users.noreply.github.com>
2026-08-23 14:07:02 +0530

    Time: 59 ms (50%), Space: 19.9 MB (65.29%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num)
+        mid = n//2
+
+        sum_diff = 0
+        mark_diff = 0
+
+        for i in range(mid):
+            if num[i] == '?':
+                mark_diff += 1
+            else:
+                sum_diff += int(num[i])
+
+        for i in range(mid, n):
+            if num[i] == '?':
+                mark_diff -= 1
+            else:
+                sum_diff -= int(num[i])
+
+        if mark_diff % 2 != 0:
+            return True
+
+        return sum_diff + (mark_diff // 2) * 9 != 0 
